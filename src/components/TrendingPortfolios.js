8e8f63573d7a0ae66b2c938873852ad7c11fdb4c
Emmanuel <emmanuel@Emmanuels-MBP-2.broadband>
2023-10-02 03:55:43 +0100

    API Migration from IEX Cloud to Rapid API's Yahoo Finance plugin

--- src/components/TrendingPortfolios.js
+++ src/components/TrendingPortfolios.js
@@ -2,6 +2,38 @@ import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
 import NumberFormat from 'react-number-format';
 import useFetch from "../services/useFetch";
 import {useNavigate} from 'react-router-dom';
+// import React from 'react';
+
+
+// This component sources data from other parts of the API and implements them in other parts of the code. 
+
+// Helper Function: renderTableRow
+// This function is used to render a single row in a table, based on the data passed into it:
+
+// It takes two parameters: the data for the row (which includes stock information) and a navigate function.
+// The navigateToDetails function inside it navigates the user to a detailed view of a particular stock when a table row is clicked.
+// It then returns a JSX representation of a table row (<tr>) where each table cell (<td>) represents a piece of stock data, like the stock's symbol, company name, previous close price, etc.
+// The NumberFormat component is used to display monetary values with a $ prefix and comma as the thousand separator.
+// The last cell changes its text color based on whether the stock's percentage change (changePercent) is positive or negative.
+// Main Component: TrendingPortfolios
+// This is the primary component:
+
+// Props: This component accepts two props: title (a string) and url (the endpoint to fetch stock data).
+
+// Fetching Data: The component uses the custom useFetch hook to fetch stock data from the provided url. This hook returns the data, any errors, and a loading state.
+
+// Loading and Error States:
+
+// If there's an error, the component throws it.
+// If the data is still being loaded, a progress bar is displayed.
+// Returned JSX:
+
+// The component returns a div element that contains:
+// A title.
+// Left and right navigation buttons (currently hidden).
+// A table with headers for stock details.
+// The body of the table (<tbody>) was meant to map over the gainersList data and render rows using the renderTableRow helper function. However, this mapping is commented out in the code you provided ({/* {gainersList.map(symbol => renderTableRow(symbol, navigate))} */}), so no data is currently being displayed in the table.
+// The styles and classes used (like btn, btn-circle, btn-sm, etc.) suggest that the app likely uses a utility-first CSS framework like Tailwind CSS.
 
 function renderTableRow(data, navigate) {
     function navigateToDetails() {
@@ -11,38 +43,49 @@ function renderTableRow(data, navigate) {
     return (
         <tr onClick={() => navigateToDetails()} className="hover:bg-primary cursor-pointer" key={data.symbol}>
             <td className='bg-transparent text-accent'>{data.symbol}</td>
-            <td className='bg-transparent'>{data.companyName}</td>
-            <td className={'bg-transparent text-neutral'}><NumberFormat value={data.previousClose} displayType={'text'}
-                                                                        thousandSeparator={true} prefix={'$'}/></td>
-            <td className='bg-transparent text-info'>
-                <NumberFormat value={data.latestPrice} displayType={'text'} thousandSeparator={true} prefix={'$'}/>
+            <td className='bg-transparent'>{data.shortName}</td>
+            <td className={'bg-transparent text-neutral'}>
+                <NumberFormat value={data.regularMarketPreviousClose} displayType={'text'}
+                              thousandSeparator={true} prefix={'$'}/>
             </td>
-            <td className='bg-transparent text-neutral'>{data.latestVolume}</td>
-            <td className='bg-transparent'><NumberFormat value={data.open} displayType={'text'}
-                                                         thousandSeparator={true}
-                                                         prefix={'$'}/></td>
-            <td className='bg-transparent'><NumberFormat value={data.close} displayType={'text'}
-                                                         thousandSeparator={true} prefix={'$'}/></td>
-            <td className={data.changePercent > 0 ? 'bg-transparent text-green' : 'bg-transparent text-pink-red'}>{data.changePercent.toFixed(2)}</td>
+            <td className='bg-transparent text-info'>
+                <NumberFormat value={data.regularMarketPrice} displayType={'text'} 
+                              thousandSeparator={true} prefix={'$'}/>
+            </td>
+            <td className='bg-transparent text-neutral'>{data.regularMarketVolume}</td>
+            <td className='bg-transparent'>
+                <NumberFormat value={data.regularMarketOpen} displayType={'text'}
+                              thousandSeparator={true} prefix={'$'}/>
+            </td>
+            <td className='bg-transparent'> 
+                <NumberFormat value={data.postMarketPrice} displayType={'text'}
+                              thousandSeparator={true} prefix={'$'}/>
+            </td>
+            <td className={data.regularMarketChangePercent > 0 
+                            ? 'bg-transparent text-green' 
+                            : 'bg-transparent text-pink-red'}>
+                {data.regularMarketChangePercent.toFixed(2)}
+            </td>
         </tr>
     )
 }
 
 export function TrendingPortfolios({title, url}) {
-
     const navigate = useNavigate();
 
-    const {data: gainersList, error, loading} = useFetch(url, "&displayPercent=true");
+    const {data: response, error, loading} = useFetch(url);
 
-    if (error) throw  error;
+    // Accessing the quotes key from the response
+    const gainersList = response?.quotes || [];
+
+    if (error) throw error;
 
     if (loading) {
         return <progress className="progress w-56"/>
     }
 
     return (
-        <div
-            className='px-8 space-y-4 mb-16 pb-8 rounded-xl shadow-lg max-w-full  overflow-x-scroll y-4 w bg-gradient-to-b from-darker-teal to-black'>
+        <div className='px-8 space-y-4 mb-16 pb-8 rounded-xl shadow-lg max-w-full overflow-x-scroll y-4 w bg-gradient-to-b from-darker-teal to-black'>
             <div className='flex justify-between pt-8'>
                 <div className='font-bold text-base'>{title}</div>
                 <div className='space-x-2 hidden'>
@@ -54,7 +97,6 @@ export function TrendingPortfolios({title, url}) {
                     </button>
                 </div>
             </div>
-
 
             <table className="table table-fixed min-w-full max-w-screen-lg bg-transparent select-none ">
                 <thead>
--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,35 +1,33 @@
-import {useEffect, useState} from "react";
+import { useState, useEffect } from 'react';
 
-const baseUrl = 'https://cloud.iexapis.com/stable/'
-const token = 'pk_eb688d810a8440bfb8470012290c8f79';
-
-export default function useFetch(url, queryParams = '') {
-    const [data, setData] = useState(null)
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(true)
+function useFetch(url) {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        async function init() {
+        async function fetchData() {
             try {
-                const response = await fetch(baseUrl + url + "?token=" + token + queryParams)
-                if (response.ok) {
-                    const json = await response.json()
-                    // console.log('response data', json)
-                    setData(json)
-                }
-                // else {
-                //     throw response;
-                // }
-            } catch (e) {
-                setError(e)
-            } finally {
-                setLoading(false)
+                let response = await fetch(url, {
+                    method: "GET",
+                    headers: {
+                        "X-RapidAPI-Key": "d75e476636msha08ea07fdb84f37p11a6ccjsnf6345c38ebe8",
+                        "X-RapidAPI-Host": "yahoo-finance15.p.rapidapi.com"
+                    }
+                });
+                let result = await response.json();
+                console.log(result)
+                setData(result);
+                setLoading(false);
+            } catch (err) {
+                setError(err);
+                setLoading(false);
             }
         }
+        fetchData();
+    }, [url]);
 
-        init();
-
-    }, [queryParams, url])
-
-    return {data, error, loading}
+    return { data, loading, error };
 }
+
+export default useFetch;
