b7b926805876384bdba1f6ee2e89bdab7388ad8b
EmmanuelSnr1 <emmanuelappiah124@hotmail.com>
2023-10-29 02:15:00 +0000

    add transaction modal made. Requests structuring to be prepared.

--- src/components/AddTransactionModal.js
+++ src/components/AddTransactionModal.js
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-function AddTransactionModal({ isOpen, onClose, refetch }) {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
+function AddTransactionModal({ isOpen, onClose, refetch, portfolioId }) {
+  const [assetType, setAssetType] = useState("SECURITY");
+  const [transactionType, setTransactionType] = useState("CREDIT");
+  const [symbol, setSymbol] = useState("");
+  const [date, setDate] = useState("");
+  const [shares, setShares] = useState("");
+  const [price, setPrice] = useState("");
+  const [fees, setFees] = useState("");
+  const [accountInfo, setAccountInfo] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [amount, setAmount] = useState("");
 
   const handleSave = async () => {
     const payload = {
-      name: name,
-      description: description,
-      // You don't need to set the 'user' field here since it's set on the server-side
+      assetType: assetType,
+      transactionType: transactionType,
+      symbol: symbol,
+      date: date,
+      shares: shares,
+      price: price,
+      fees: fees,
+      accountInfo: accountInfo,
+      // Add other fields as needed...
     };
 
     try {
-      await axios.post("http://localhost:8090/api/portfolio", payload, {
-        withCredentials: true,
-      });
-      onClose(); // Close the modal after successful creation
-      // Optionally, you can also refetch the portfolio data or update the local state to reflect the changes
+      await axios.post(
+        `http://localhost:8090/api/portfolio/${portfolioId}/transactions`,
+        [payload],
+        {
+          withCredentials: true,
+        }
+      );
+      onClose();
+      refetch();
     } catch (error) {
       console.error("Error creating transaction:", error);
     }
-    refetch();
   };
 
   return (
@@ -38,28 +55,146 @@ function AddTransactionModal({ isOpen, onClose, refetch }) {
 
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Name</span>
+            <span className="label-text">Asset Type</span>
           </label>
-          <input
-            type="text"
-            className="input input-bordered"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Enter Transaction name"
-          />
+          <select
+            className="select select-bordered"
+            value={assetType}
+            onChange={(e) => setAssetType(e.target.value)}
+          >
+            <option value="SECURITY">SECURITY</option>
+            <option value="CASH">CASH</option>
+          </select>
         </div>
 
+        {assetType === "SECURITY" && (
+          <>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Symbol</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered"
+                value={symbol}
+                onChange={(e) => setSymbol(e.target.value)}
+                placeholder="Enter stock symbol (e.g., AAPL)"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Transaction Type</span>
+              </label>
+              <select
+                className="select select-bordered"
+                value={transactionType}
+                onChange={(e) => setTransactionType(e.target.value)}
+              >
+                <option value="BUY_LONG">BUY LONG</option>
+                <option value="SELL_LONG">SELL LONG</option>
+                <option value="SELL_SHORT">SELL SHORT</option>
+                <option value="BUY_SHORT">BUY SHORT</option>
+                <option value="DEPOSIT">DEPOSIT</option>
+                <option value="BUY">BUY</option>
+                <option value="SELL">SELL</option>
+              </select>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Shares</span>
+              </label>
+              <input
+                type="number"
+                className="input input-bordered"
+                value={shares}
+                onChange={(e) => setShares(e.target.value)}
+                placeholder="Enter number of shares"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Price</span>
+              </label>
+              <input
+                type="number"
+                className="input input-bordered"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                placeholder="Enter price"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Fees</span>
+              </label>
+              <input
+                type="number"
+                className="input input-bordered"
+                value={fees}
+                onChange={(e) => setFees(e.target.value)}
+                placeholder="Enter Fees"
+              />
+            </div>
+            {/* ... other SECURITY-specific fields ... */}
+          </>
+        )}
+
+        {assetType === "CASH" && (
+          <>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Account Info</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered"
+                value={accountInfo}
+                onChange={(e) => setAccountInfo(e.target.value)}
+                placeholder="Enter account name/description"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Transaction Type</span>
+              </label>
+              <select
+                className="select select-bordered"
+                value={transactionType}
+                onChange={(e) => setTransactionType(e.target.value)}
+              >
+                <option value="OPEN">OPEN</option>
+                <option value="CREDIT">CREDIT</option>
+                <option value="DEBIT">DEBIT</option>
+              </select>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Amount</span>
+              </label>
+              <input
+                type="number"
+                className="input input-bordered"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Enter Amount"
+              />
+            </div>
+            {/* ... other CASH-specific fields ... */}
+          </>
+        )}
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Description</span>
+            <span className="label-text">Date</span>
           </label>
-          <textarea
-            className="textarea textarea-bordered"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Enter Transaction description"
-          ></textarea>
+          <input
+            type="date"
+            className="input input-bordered"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
         </div>
+        {/* ... other common fields ... */}
 
         <div className="modal-action">
           <button className="btn" onClick={handleSave}>
--- src/components/PortfolioGraph.js
+++ src/components/PortfolioGraph.js
@@ -49,7 +49,7 @@ export function PortfolioGraph() {
                 tabIndex={0}
                 className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <li>
+                <li className="text-accent">
                   <a onClick={() => setAddPortfolioModalOpen(true)}>
                     {" "}
                     Add New Portfolio +{" "}
